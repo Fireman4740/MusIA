ba73d206125547ed07aa0b3fb60f0dbc2bb56c39
Fireman4740 <mathis.carlesso@viacesi.fr>
2023-06-05 23:19:44 +0200

    stat fini

--- statistique.py
+++ statistique.py
@@ -19,7 +19,7 @@ card_values = {
 deck = ['Roi', 'Cavalier', 'Valet', '7', '6', '5', '4', '3', '2', 'As'] * 4
 
 # Define the given hand
-given_hand = ['Roi', 'Roi', '2', 'As']
+given_hand = ['Roi', 'Roi', 'Cavalier', 'Valet']
 
 for card in given_hand:
     deck.remove(card)
@@ -95,3 +95,46 @@ print(f"The probability of winning with the given hand in the 'Paires' phase is 
 
 print(f"The probability of winning with the given hand is  {probabilityGrand:.2f}% at the grand ")
 print(f"The probability of winning with the given hand is  {probabilityPetit:.2f}% at the petit ")
+# Define the values of the cards for the "Jokoa" phase
+card_values_jokoa = {
+    'Roi': 10,
+    'Cavalier': 10,
+    'Valet': 10,
+    '7': 7,
+    '6': 6,
+    '5': 5,
+    '4': 4,
+    '3': 3,
+    '2': 2,
+    'As': 1
+}
+
+# Define the order of the sums for the "Jokoa" phase
+jokoa_order = [31, 32, 40, 37, 36, 35, 34, 33]
+
+# Calculate the sum of the given hand
+given_hand_sum = sum(card_values_jokoa[card] for card in given_hand)
+
+# Check if the player has "jokoa"
+if given_hand_sum < 31:
+    print("The player does not have 'jokoa'. The probability of winning is 0%.")
+else:
+    # Generate all possible hands
+    possible_hands = list(itertools.combinations(deck, 4))
+
+    # Filter the possible hands to keep only those with a sum of 31 or more
+    possible_hands = [hand for hand in possible_hands if sum(card_values_jokoa[card] for card in hand) >= 31]
+
+    # Initialize the count of wins
+    winsJ = 0
+
+    # Compare the given hand with all possible hands
+    for hand in possible_hands:
+        hand_sum = sum(card_values_jokoa[card] for card in hand)
+        if jokoa_order.index(given_hand_sum) < jokoa_order.index(hand_sum):
+            winsJ += 1
+
+    # Calculate the probability of winning
+    probability = winsJ / len(possible_hands)*100
+
+    print(f"The probability of winning with the given hand in the 'Jokoa' phase is {probability:.2f}%")
